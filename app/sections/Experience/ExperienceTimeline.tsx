4e233440716c0b5ef46ea8e752b30e1864c8e266
Valdis <vgbukalders@gmail.com>
2025-12-26 14:49:48 +0200

    timeline progress

--- app/sections/Experience/ExperienceTimeline.tsx
+++ app/sections/Experience/ExperienceTimeline.tsx
@@ -14,52 +14,90 @@ import './timeline-style.css';
 
 function ExperienceTimeline() {
   return (
-    <div className="w-[min(900px,90dvw)]">
+    <div className="w-[min(1400px,90dvw)]">
       <VerticalTimeline lineColor="#888888">
-        {experiences.map((exp, index) => (
-          <VerticalTimelineElement
-            date={`${exp.startDate.toLocaleDateString(undefined, {
+        {experiences.map((exp, index) => {
+          const endDate = exp.endDate ?? new Date();
+          const monthsWorked =
+            endDate.getMonth() -
+            exp.date.getMonth() +
+            1 +
+            12 * (endDate.getFullYear() - exp.date.getFullYear());
+
+          const years = Math.floor(monthsWorked / 12);
+          const months = monthsWorked % 12;
+          let displayedYears = '';
+          let displayedMonths = '';
+          if (years > 1) displayedYears = `${years} yrs`;
+          else if (years === 1) displayedYears = `1 yr`;
+
+          if (months > 1) displayedMonths = `${months} mos`;
+          else if (months === 1) displayedMonths = `1 mo`;
+
+          const displayedTenure = [displayedYears, displayedMonths]
+            .filter(Boolean)
+            .join(' ');
+
+          const displayedDuration = `${exp.date.toLocaleDateString(undefined, {
+            year: 'numeric',
+            month: 'short',
+          })} - ${
+            exp.endDate?.toLocaleDateString(undefined, {
               year: 'numeric',
               month: 'short',
-            })} - ${
-              exp.endDate?.toLocaleDateString(undefined, {
-                year: 'numeric',
-                month: 'short',
-              }) ?? 'Present'
-            }`}
-            className="group"
-            dateClassName="pointer-events-none px-2!"
-            key={index}
-            icon={
-              <Link
-                href={exp.companyUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  className={`size-full cursor-pointer overflow-hidden rounded-full border-2 border-white transition-opacity hover:border-[#ff1f8f] ${styles['neon-box-shadow']}`}
-                  src={exp.companyLogo}
-                  alt={`${exp.company} logo`}
-                />
-              </Link>
-            }
-            iconClassName="shadow-none!"
-            contentArrowStyle={{ display: 'none' }}
-          >
-            <div className="flex flex-col">
-              <span className="mb-1">
-                <span className="text-lg">{exp.company} </span>
-                <span className="text-xs font-medium italic opacity-60">
-                  {exp.type}
+            }) ?? 'Present'
+          } (${displayedTenure})`;
+
+          const displayedDate = exp.date.toLocaleDateString(undefined, {
+            year: 'numeric',
+            month: 'short',
+          });
+
+          return (
+            <VerticalTimelineElement
+              date={
+                exp.entryType === 'duration' ? displayedDuration : displayedDate
+              }
+              className="group"
+              dateClassName="pointer-events-none px-2!"
+              key={index}
+              icon={
+                <Link
+                  href={exp.imageUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image
+                    className={`size-full cursor-pointer overflow-hidden rounded-full border-2 border-white transition-opacity hover:border-[#ff1f8f] ${styles['neon-box-shadow']}`}
+                    src={exp.image}
+                    alt={`${exp.subTitle} logo`}
+                  />
+                </Link>
+              }
+              iconClassName="shadow-none!"
+              contentArrowStyle={{ display: 'none' }}
+            >
+              <div className="flex flex-col">
+                <span className="mb-1">
+                  <span className="text-lg">{exp.subTitle} </span>
+                  {exp.extraInfo && (
+                    <span className="text-xs font-medium italic opacity-60">
+                      {exp.extraInfo}
+                    </span>
+                  )}
                 </span>
-              </span>
-              <span className="text-2xl font-medium">{exp.position}</span>
-              {exp.description.map((desc, i) => (
-                <span key={i}>{desc}</span>
-              ))}
-            </div>
-          </VerticalTimelineElement>
-        ))}
+                <span className="text-2xl font-medium">{exp.title}</span>
+                <div className="flex flex-col gap-2">
+                  {exp.description?.map((desc, i) => (
+                    <span key={i} className="indent-2">
+                      {desc}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </VerticalTimelineElement>
+          );
+        })}
       </VerticalTimeline>
     </div>
   );
--- app/sections/Experience/data.ts
+++ app/sections/Experience/data.ts
@@ -2,57 +2,91 @@ import type { StaticImageData } from 'next/image';
 
 import dhanvantariLogo from '@/public/dhanvantari.jpg';
 import giraffe360Logo from '@/public/giraffe.png';
+import universityLogo from '@/public/lu.png';
 
 type ExperienceEntry = {
-  company: string;
-  companyLogo: StaticImageData;
-  companyUrl: string;
-  position: string;
-  type: 'Full time' | 'Part time' | 'Internship';
-  startDate: Date;
-  endDate: Date | null;
-  description: string[];
+  subTitle: string;
+  image: StaticImageData;
+  imageUrl: string;
+  title: string;
+  extraInfo?: string;
+  entryType: 'duration' | 'event';
+  date: Date;
+  endDate?: Date;
+  description?: string[];
 };
 
-export const experiences: ExperienceEntry[] = [
-  {
-    company: 'Dhanvantari Solutions',
-    companyLogo: dhanvantariLogo,
-    companyUrl: 'https://www.dsolutions.lv/',
-    position: 'Full Stack Developer',
-    type: 'Internship',
-    startDate: new Date('2021-07-01'),
-    endDate: new Date('2021-08-31'),
-    description: ['lorem ipsum dolor sit amet consectetur adipiscing elit'],
-  },
-  {
-    company: 'Dhanvantari Solutions',
-    companyLogo: dhanvantariLogo,
-    companyUrl: 'https://www.dsolutions.lv/',
-    position: 'Full Stack Developer',
-    type: 'Part time',
-    startDate: new Date('2021-08-31'),
-    endDate: new Date('2022-06-06'),
-    description: ['lorem ipsum dolor sit amet consectetur adipiscing elit'],
-  },
-  {
-    company: 'Giraffe360',
-    companyLogo: giraffe360Logo,
-    companyUrl: 'https://www.giraffe360.com/',
-    position: 'Frontend Developer',
-    type: 'Full time',
-    startDate: new Date('2022-06-06'),
-    endDate: new Date('2024-06-01'),
-    description: ['lorem ipsum dolor sit amet consectetur adipiscing elit'],
-  },
-  {
-    company: 'Giraffe360',
-    companyLogo: giraffe360Logo,
-    companyUrl: 'https://www.giraffe360.com/',
-    position: 'Senior Frontend Developer',
-    type: 'Full time',
-    startDate: new Date('2024-06-01'),
-    endDate: null,
-    description: ['lorem ipsum dolor sit amet consectetur adipiscing elit'],
-  },
-] as const;
+export const experiences: ExperienceEntry[] = (
+  [
+    {
+      subTitle: 'University of Latvia',
+      image: universityLogo,
+      imageUrl:
+        'https://www.lu.lv/studijas/studiju-programmas/bakalaura-limena-studijas/datorzinatnes/',
+      title: 'Bachelor of Computer Science',
+      extraInfo: 'Started studies',
+      entryType: 'event',
+      date: new Date('2020-09-01'),
+    },
+    {
+      subTitle: 'Dhanvantari Solutions',
+      image: dhanvantariLogo,
+      imageUrl: 'https://www.dsolutions.lv/',
+      title: 'Full Stack Developer',
+      extraInfo: 'Internship',
+      entryType: 'duration',
+      date: new Date('2021-07-01'),
+      endDate: new Date('2021-08-31'),
+      description: [
+        'My first professional experience as a developer, I got the opportunity to work at this digital agency through my university mentorship program.',
+        'The internship had me working on a laravel project, where I also got to learn some React.js basics, even though it was still using the class component system, which sparked my initial intereset in frontend development.',
+      ],
+    },
+    {
+      subTitle: 'Dhanvantari Solutions',
+      image: dhanvantariLogo,
+      imageUrl: 'https://www.dsolutions.lv/',
+      title: 'Full Stack Developer',
+      entryType: 'duration',
+      extraInfo: 'Part time',
+      date: new Date('2021-08-31'),
+      endDate: new Date('2022-06-06'),
+      description: [
+        'After my initial internship, I continued working at the agency part-time whilst studying at university, also completing my university\'s "mandatory internship" (6 months) here.',
+        'During this time, I got to work on many different projects, but most were backend focused and were heavily reliant on aging technologies like jQuery and Drupal, which definitely did not match my interests at the time, so I started looking into other opportunities.',
+        'I did, however, get to dip my toes into React Native development during the end of my time here, which further solidified my interest in frontend development. (Especially since the codebase was using modern react instead of the class component system)',
+      ],
+    },
+    {
+      subTitle: 'Giraffe360',
+      image: giraffe360Logo,
+      imageUrl: 'https://www.giraffe360.com/',
+      title: 'Frontend Developer',
+      extraInfo: 'Full time',
+      entryType: 'duration',
+      date: new Date('2022-06-06'),
+      endDate: new Date('2024-06-01'),
+      description: ['lorem ipsum dolor sit amet consectetur adipiscing elit'],
+    },
+    {
+      subTitle: 'University of Latvia',
+      image: universityLogo,
+      imageUrl:
+        'https://www.lu.lv/studijas/studiju-programmas/bakalaura-limena-studijas/datorzinatnes/',
+      title: 'Bachelor of Computer Science',
+      extraInfo: 'Finished studies',
+      entryType: 'event',
+      date: new Date('2024-05-31'),
+    },
+    {
+      subTitle: 'Giraffe360',
+      image: giraffe360Logo,
+      imageUrl: 'https://www.giraffe360.com/',
+      title: 'Senior Frontend Developer',
+      entryType: 'duration',
+      extraInfo: 'Full time',
+      date: new Date('2024-06-01'),
+      description: ['lorem ipsum dolor sit amet consectetur adipiscing elit'],
+    },
+  ] satisfies ExperienceEntry[]
+).sort((a, b) => a.date.getTime() - b.date.getTime());
