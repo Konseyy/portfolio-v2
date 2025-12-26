3e1e2b50f571470b9c13be1190d80ab48a1751f5
Valdis <vgbukalders@gmail.com>
2025-12-26 16:45:16 +0200

    collapsable timeline

--- app/sections/Experience/ExperienceTimeline.tsx
+++ app/sections/Experience/ExperienceTimeline.tsx
@@ -3,12 +3,15 @@
 import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from 'react-vertical-timeline-component';
 
 import { experiences } from './data';
+import CollapseIcon from '../../components/icons/CollapseIcon';
+import ExpandIcon from '../../components/icons/ExpandIcon';
 import styles from '@/app/css/neon-box-shadow.module.css';
 
 import './timeline-style.css';
@@ -16,12 +19,28 @@ import './timeline-style.css';
 const LOCALE = 'en-150' as const;
 
 function ExperienceTimeline() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <div className="flex flex-col">
-      <div className="flex justify-start @min-[1170px]:justify-center">
+      <div className="relative mt-5 mb-1 flex items-center justify-start gap-2 @min-[1170px]:justify-center">
         <span className="m-1 text-3xl @md:text-4xl @lg:text-5xl">
           The timeline
         </span>
+        <button
+          className={clsx(
+            styles['neon-box-shadow'],
+            'absolute top-2 right-2 size-6 w-fit cursor-pointer rounded-sm p-1 text-white',
+            '@min-[1170px]:relative @min-[1170px]:top-0 @min-[1170px]:right-0 @min-[1170px]:ml-2'
+          )}
+          onClick={() => setCollapsed((o) => !o)}
+        >
+          {collapsed ? (
+            <ExpandIcon className="size-full fill-white" />
+          ) : (
+            <CollapseIcon className="size-full fill-white" />
+          )}
+        </button>
       </div>
       <div className="w-[min(1400px,90dvw)]">
         <VerticalTimeline>
@@ -104,13 +123,18 @@ function ExperienceTimeline() {
                     )}
                   </span>
                   <span className="text-2xl font-medium">{exp.title}</span>
-                  <div className="flex flex-col gap-2">
-                    {exp.description?.map((desc, i) => (
-                      <span key={i} className="indent-2 text-sm @md:text-base">
-                        {desc}
-                      </span>
-                    ))}
-                  </div>
+                  {!collapsed && (
+                    <div className="flex flex-col gap-2">
+                      {exp.description?.map((desc, i) => (
+                        <span
+                          key={i}
+                          className="indent-2 text-sm @md:text-base"
+                        >
+                          {desc}
+                        </span>
+                      ))}
+                    </div>
+                  )}
                 </div>
               </VerticalTimelineElement>
             );
